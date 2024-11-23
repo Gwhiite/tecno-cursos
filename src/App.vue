17370b176f2f11b6423d8e65773bcbda8f697435
gwhiite <guibrancodecampos@gmail.com>
2024-11-23 03:22:27 -0300

    feat: final project

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<script>
+import Header from "../src/components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <Header />
+    <router-view class="container" />
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0px;
+  box-sizing: border-box;
+  font-family: "Avenir", sans-serif;
+  color: #234;
+}
+ul {
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+}
+a {
+  font-weight: bold;
+  text-decoration: none;
+  color: #234;
+}
+a.router-link-active {
+  color: #4b8;
+}
+
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.v-enter {
+  opacity: 0;
+  transform: translate3d(-20px, 0, 0);
+}
+
+.v-enter-active {
+  transition: all 0.3s;
+}
+
+img {
+  max-width: 100%;
+}
+
+.conteudo {
+  display: grid;
+  grid-template-columns: 1fr minmax(280px, 400px);
+  grid-gap: 30px;
+}
+
+@media screen and (max-width: 700px) {
+  .conteudo {
+    display: block;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,60 @@
+<script>
+import fetchData from "../mixins/fetchData.js";
+import PageLoading from "../components/PageLoading.vue";
+
+export default {
+  components: {
+    PageLoading,
+  },
+  name: "Home",
+  mixins: [fetchData],
+  created() {
+    this.fetchData("/home");
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div v-if="loading">
+      <PageLoading />
+    </div>
+    <transition>
+      <div v-if="api" class="conteudo">
+        <div>
+          <h1>Sobre a {{ api.titulo }}</h1>
+          <p>{{ api.descricao }}</p>
+          <router-link tag="button" class="btn-cursos" to="/cursos"
+            >Cursos</router-link
+          >
+          <div>
+            <h2>Avaliações</h2>
+            <ul>
+              <li v-for="avaliacao in api.avaliacoes" :key="avaliacao.nome">
+                <p>{{ avaliacao.nome }}</p>
+                <p>{{ avaliacao.descricao }}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img src="../assets/aprender.png" alt="Aprenda web design" />
+      </div>
+    </transition>
+  </div>
+</template>
+
+<style scoped>
+.btn-cursos {
+  border: none;
+  background: #4b8;
+  border-radius: 4px;
+  color: white;
+  cursor: pointer;
+  box-shadow: 0 4px 2px rgba(0, 0, 0, 0.1);
+  padding: 15px 40px;
+  font-size: 1rem;
+  margin-bottom: 40px;
+  margin-top: 40px;
+  font-family: "Avenir", sans-serif;
+}
+</style>
